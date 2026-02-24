2daca9200fc91ffbd499b51248035f8a1d55c80f
CodeMonk6 <sourabh.node@gmail.com>
2026-02-23 22:19:21 -0600

    Create __init__.py for package initialization and main exports

--- neural_alpha/__init__.py
+++ neural_alpha/__init__.py
@@ -1,7 +1,4 @@
 # Package Initialization
 
-"""
-Neural Alpha package initialization.
-"""
-
-__all__ = ['module1', 'module2', 'module3']  # Update with the actual module names
+# Main Exports
+__all__ = ['NeuralNetwork', 'Optimizer', 'LossFunction']
